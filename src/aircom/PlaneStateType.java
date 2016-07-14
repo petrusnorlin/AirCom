5b3d2640438e18573acb8662da6f3777dd88dde7
Joel-javadev <joeld.javadev@gmail.com>
2016-07-14 15:36:30 +0200

    AirplaneThread första försöket

--- src/aircom/PlaneStateType.java
+++ src/aircom/PlaneStateType.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aircom;
 
 /**
@@ -10,5 +5,5 @@ package aircom;
  * @author User
  */
 public enum PlaneStateType {
-    PREPAIR,INAIR,LANDED
+    BOARDING, TAKING_OFF, FLYING, LANDING, DESEMBARKING, EMPTY
 }
